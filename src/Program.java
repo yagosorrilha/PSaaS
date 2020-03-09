737d017649a6ab810d1d4d03d8827c61cd7aaa56
Yago Sorrilha <yagosorrilha@hotmail.com>
2020-03-09 03:37:39 -0300

    Incluindo projeto java

---
+++ src/Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Iniciando fase de levantamento de requisitos");
+
+	}
+
+}
