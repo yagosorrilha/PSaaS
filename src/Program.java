a4178f71f388f52640af221b4f17909dbd6c2045
Yago Sorrilha <yagosorrilha@hotmail.com>
2020-03-18 23:30:40 -0300

    Inclusão da elicitação de requisitos

--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
-		
+
 		System.out.println("Iniciando fase de levantamento de requisitos");
+		System.out.println("Responsáveis: Engenheiro de Software e Analista de Negócios");
+		System.out.println("Digite 0 para encerrar a fase de requisitos");
+
+		String tipoRequisito = "", descRequisito;
+		int contRequisitos=0;
+		
+		ArrayList<Requisitos> requisitos = new ArrayList<>();
+
+		while (tipoRequisito != "fim") {
+			
+			Requisitos requisito = new Requisitos();
+			System.out.println("Tipo de requisito: Funcional, NFuncional, Dominio ou Dados?");
+			tipoRequisito = teclado.nextLine();
+			requisito.setTipoRequisito(tipoRequisito);
+			System.out.println("Descreva o requisito: ");
+			descRequisito = teclado.nextLine();
+			requisito.setRequisito(descRequisito);
+			
+			requisitos.add(requisito);
+			
+		}
+		
+		System.out.println("Elicitação de requisitos finalizada!");
+		
 
 	}
 
---
+++ src/Requisitos.java
@@ -0,0 +1,23 @@
+
+public class Requisitos {
+
+	private String tipoRequisito, requisito;
+	
+	String getTipoRequisito() {
+		return this.tipoRequisito;
+	}
+	
+	void setTipoRequisito(String tipoRequisito) {
+		this.tipoRequisito = tipoRequisito;
+	}
+	
+	String getRequisito() {
+		return this.requisito;
+	}
+	
+	void setRequisito(String requisito) {
+		this.requisito = requisito;
+	}
+	
+	
+}
